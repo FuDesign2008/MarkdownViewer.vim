9f53602a11e8fda0b4f624e82f20384bd0938bc8
fudesign2008 <fudesign2008@163.com>
2019-11-21 16:12:27 +0800

    modified:   after/ftplugin/markdown/viewer.js

--- after/ftplugin/markdown/viewer.js
+++ after/ftplugin/markdown/viewer.js
@@ -17,9 +17,33 @@ if (!hljs) {
   console.log('Error: no highlight.js')
   process.exit(1)
 }
+const markdownCompiler = markdownIt()
 
+const options = {
 
-const markdownCompiler = markdownIt({
+  // Enable HTML tags in source
+  html: true,
+  // Use '/' to close single tags (<br />).
+  // This is only for full CommonMark compatibility.
+  xhtmlOut: true,
+  // Convert '\n' in paragraphs into <br>
+  breaks: false,
+  // CSS language prefix for fenced blocks. Can be
+  // useful for external highlighters.
+  langPrefix: 'language-',
+  // Autoconvert URL-like text to links
+  linkify: true,
+  // Enable some language-neutral replacement + quotes beautification
+  typographer: true,
+  // Double + single quotes replacement pairs, when typographer enabled,
+  // and smartquotes on. Could be either a String or an Array.
+  //
+  // For example, you can use '«»„“' for Russian, '„“‚‘' for German,
+  // and ['«\xA0', '\xA0»', '‹\xA0', '\xA0›'] for French (including nbsp).
+  quotes: '“”‘’',
+  // Highlighter function. Should return escaped HTML,
+  // or '' if the source string is not changed and should be escaped externally.
+  // If result starts with <pre... internal wrapper is skipped.
   highlight(str, lang) {
     if (lang === 'mermaid'
         || str.match(/^sequenceDiagram/)
@@ -42,8 +66,10 @@ const markdownCompiler = markdownIt({
     const escapedStr = markdownCompiler.utils.escapeHtml(str)
     return `<pre class="hljs"><code>${escapedStr}</code></pre>`
   },
-})
+}
 
+
+markdownCompiler.set(options)
 
 fs.readFile(filePath, {
   encoding: 'utf-8',
